a36bf2c7d0d53678401c0bcc17fa6542ba48477b
fmgwfwt <132745147+fmgwfwt@users.noreply.github.com>
2023-08-29 14:13:01 +0300

    Update Product.java

--- demo/src/main/java/com/example/demo/domain/product/Product.java
+++ demo/src/main/java/com/example/demo/domain/product/Product.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.NotBlank;
 
 
 @Data
-@Document(collation = "products")
+@Document(collection = "products")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
