e919ca0ebcb50a1acd19a7013be4b46d5aab7778
fmgwfwt <132745147+fmgwfwt@users.noreply.github.com>
2023-08-29 14:33:34 +0300

    Create corsConfig.java

---
+++ demo/src/main/java/com/example/demo/security/corsConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+@Configuration
+public class corsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(Arrays.asList("http://fatihdemir.com"));
+        configuration.setAllowedHeaders(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
