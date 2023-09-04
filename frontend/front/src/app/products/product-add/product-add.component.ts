18bee0fbd8f0a83a52863260c65997e48957ffbe
fmgwfwt <132745147+fmgwfwt@users.noreply.github.com>
2023-09-04 09:20:42 +0300

    Update product-add.component.ts

--- frontend/front/src/app/products/product-add/product-add.component.ts
+++ frontend/front/src/app/products/product-add/product-add.component.ts
@@ -30,14 +30,13 @@ export class ProductAddComponent {
     );
   }
   handleFileInput(event: any) {
-    const file = event.target.files[0]; // Get the first selected file
+    const file = event.target.files[0];
     if (file) {
-      // Read the selected file as a data URL
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
         this.image = reader.result as string;
-        this.newProduct.productImagePath = this.image; // Set the productImagePath
+        this.newProduct.productImagePath = this.image; 
       };
     }
   }
